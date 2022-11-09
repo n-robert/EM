2b9785cdf97fa22a02e524ccacbbb2a2b9ffe568
n-robert <nrobert@mail.ru>
2022-11-09 13:01:01 +0300

    Refactoring

--- app/Http/Middleware/ForceSSL.php
+++ app/Http/Middleware/ForceSSL.php
@@ -19,7 +19,7 @@ class ForceSSL
     public function handle(Request $request, Closure $next)
     {
         if (!$request->secure() && !app()->environment('local')) {
-            dd('https://' . $request->getHost() . $request->getRequestUri());
+            dd($request->getScheme());
             return redirect()->to('https://' . $request->getHost() . $request->getRequestUri(), 302);
         }
         return $next($request);
