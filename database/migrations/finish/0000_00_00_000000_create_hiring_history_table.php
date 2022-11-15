7d1e4e71c8e0b68b9d7a87baf8f1db533ead98a0
n-robert <nrobert@mail.ru>
2022-11-15 15:47:51 +0300

    Refactoring

--- database/migrations/finish/0000_00_00_000000_create_hiring_history_table.php
+++ database/migrations/finish/0000_00_00_000000_create_hiring_history_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEmployeeHiringHistoryTable extends Migration
+class CreateHiringHistoryTable extends Migration
 {
     /**
      * Run the migrations.
