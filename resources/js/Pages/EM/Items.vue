6924dcb14d2eddb254fa77c87d060b1319fd1deb
n-robert <nrobert@mail.ru>
2022-11-16 11:01:58 +0300

    Refactoring

--- resources/js/Pages/EM/Items.vue
+++ resources/js/Pages/EM/Items.vue
@@ -18,8 +18,8 @@
 
                     <template #content>
                         <div v-for="(elements, field) in filters" class="p-2 my-4 border rounded-lg w-full">
-                            <div class="font-bold text-indigo-600">
-                                {{ elements.length > 1 && field && __(field).ucFirst() }}
+                            <div v-if="elements.length > 1" class="font-bold text-indigo-600">
+                                {{ field && __(field).ucFirst() }}
                             </div>
 
                             <div v-for="element in elements" class="inline-flex">
