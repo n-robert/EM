6d6391dea0b8ba6384d5377927fd6b1f15ebc4fe
n-robert <nrobert@mail.ru>
2022-12-18 11:57:30 +0300

    Test StaffSeeder schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(new \Database\Seeders\StaffSeeder)->hourlyAt(35);
+        $schedule->job(new \Database\Seeders\StaffSeeder)->hourlyAt(5);
     }
 
     /**
