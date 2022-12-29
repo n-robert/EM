7df2b85f6511ee7d076ba6ec9851d4f0ced1e65e
n-robert <nrobert@mail.ru>
2022-12-29 21:35:34 +0300

    Refactoring

--- resources/js/Pages/EM/FieldSet.vue
+++ resources/js/Pages/EM/FieldSet.vue
@@ -41,7 +41,7 @@
             <span :class="leftColumn"></span>
             <span :class="rightColumn">
                 <e-m-button
-                    v-if="field.deletable"
+                    v-if="field.deletable || page.props.isAdmin"
                     type="button"
                     :originalText="__('Remove ' + name)"
                     customClass="hover:text-white hover:bg-indigo-500"
@@ -85,6 +85,7 @@ export default {
     data() {
         return {
             isNotFields: ['type', 'show', 'repeatable', 'deletable'],
+            page: this.$page,
         };
     },
 
