d89b76585c918329b9abff9159c2560efd07410e
n-robert <nrobert@mail.ru>
2023-01-01 14:19:09 +0300

    Refactoring

--- resources/js/Pages/EM/AppLayout.vue
+++ resources/js/Pages/EM/AppLayout.vue
@@ -9,7 +9,7 @@
                     <div class="flex justify-between h-16">
                         <div class="flex">
                             <!-- Logo -->
-                            <div class="pt-1 flex-shrink-0 flex items-center">
+                            <div class="pt-4 flex-shrink-0 flex items-center">
                                 <inertia-link :href="'/'">
                                     <em-application-mark :class="'h-9'"/>
                                 </inertia-link>
