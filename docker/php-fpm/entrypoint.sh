8ba7e4568b8ce0fe7d34993068c1b3ffcd49193b
n-robert <nrobert@mail.ru>
2023-02-17 18:32:10 +0300

    Refactoring

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -3,5 +3,4 @@
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chgrp -R www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R ug+rwx /var/www/storage /var/www/bootstrap/cache
-composer install
-npm install
+
